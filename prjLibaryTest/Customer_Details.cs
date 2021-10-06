2d7c5eed683c4f668d88e86edd8a96bafc03243f
Humbleasf <81228161+Humbleasf@users.noreply.github.com>
2021-10-06 11:46:43 +0200

    Add files via upload

---
+++ prjLibaryTest/Customer_Details.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace prjLibaryTest
+{
+    public class Customer_Details
+    {
+
+        public Boolean CheckEmail(String strInput)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(strInput);
+                return addr.Address == strInput;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public Boolean AddUserToDatabase(UserDetail user)
+        {
+            Connection c = new Connection();
+            bool bcheck = false;
+            SqlConnection conn = new SqlConnection(@"" + c.connection);
+                       
+            string sql = "INSERT INTO User (name, surname, password, email) values (@name, @surname, @password, @email)";
+            conn.Open(); 
+            SqlCommand command = new SqlCommand(sql, conn); 
+            command.Parameters.AddWithValue("@name", user.getName());
+            command.Parameters.AddWithValue("@surname", user.getSurname());
+            command.Parameters.AddWithValue("@password", user.getPassword());
+            command.Parameters.AddWithValue("@email", user.getEmail());
+            conn.Open();
+            int i = command.ExecuteNonQuery();
+            conn.Close();
+
+            if (i == -1)
+            {
+                bcheck = true;
+            }
+            return bcheck;
+        }
+        public UserDetail HashPassword(UserDetail user)
+        {
+            String strPassword = user.getPassword();
+            user.setPassword(CreateMD5(strPassword));
+            return user;
+            
+        }
+        public String GetWelcomeMessage(UserDetail user)
+        {
+            string strName = user.getName();
+            string strSurname = user.getSurname();
+            string strWelcome = "Welcome " + strName + " " + strSurname;
+            return strWelcome;
+        }
+        public Boolean CheckIfUserInDatabase(UserDetail user)
+        {
+            bool bcheck = false;
+            SqlConnection conn = new SqlConnection();
+
+            string sql = "Select * FROM User where email = @email";
+            conn.Open();
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@email", user.getEmail());
+            conn.Open();
+            int i = command.ExecuteNonQuery();
+            conn.Close();
+
+            if (i >= 0)
+            {
+                bcheck = true;
+            }
+            return bcheck;
+        }
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ prjLibaryTest/UserDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjLibaryTest
+{
+    public class UserDetail
+    {
+        String Name, Surname, Password, Email;
+        public void setName(String Name)
+        {
+            this.Name = Name;
+        }
+        public void setSurname(String Surname)
+        {
+            this.Surname = Surname;
+        }
+        public void setPassword(String Password)
+        {
+            this.Password = Password;
+        }
+        public void setEmail(String Email)
+        {
+            this.Email = Email;
+        }
+        public String getName()
+        {
+            return this.Name;
+        }
+        public String getSurname()
+        {
+            return this.Surname;
+        }
+        public String getPassword()
+        {
+            return this.Password;
+        }
+        public String getEmail()
+        {
+            return this.Email;
+        }
+
+    }
+}
